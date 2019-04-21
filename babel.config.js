9378c970860fe834a0877b7297b734e895fb6aa6
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2019-04-21 21:02:30 +0530

    Linted and basic bar chart added, need to configure it 4

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
-let useBuiltIns = false;
-
-if (process.env.MODE === 'site') {
-  useBuiltIns = 'usage';
-}
-
 module.exports = {
   presets: [
-    ['@vue/app', { useBuiltIns }]
+    ['@vue/app']
   ]
 };
