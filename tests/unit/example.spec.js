4376c485883c2a1ef2bab3d84f9f0aaf4c70e7e9
Balakrishna Prasad Ganne <balkripra.1996@gmail.com>
2019-04-22 11:25:01 +0530

    Added mocha + chai instead of jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,13 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'Valar Morghulis'
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "Valar Morghulis";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    });
+    expect(wrapper.text()).to.include(msg);
+  });
+});
